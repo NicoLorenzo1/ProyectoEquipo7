3b6d1d2e21e6fe9fc538db78a7655e1d234ad638
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-06-01 12:16:45 -0300

    Clases realizadas

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Library;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("user");
+            User user2 = new User("user1");
+            User user4 = new User("user4");
+
+            //Console.WriteLine(User.users["user"]);
+           Console.WriteLine(user.Id);
+           Console.WriteLine(user2.Id);
+           Console.WriteLine(user4.Id);
+
+            
+        }
+    }
+}
