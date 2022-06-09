bfcdbd97b371e52aab23490a0aaab5b7b73fa246
José Varela <jose@lacnic.net>
2022-06-09 16:27:31 -0300

    Board v2

--- src/Library/Board.cs
+++ src/Library/Board.cs
@@ -13,19 +13,19 @@ namespace Proyecto
         {
             "/","A","B","C","D","E","F","G","H","I","J"
         };
-        private static List<string> num_Filas=new List<string>()
+        private static List<string> row_Num = new List<string>()
         {
             "1","2","3","4","5","6","7","8","9","10"
         };
-        private static List<string> lista_Caracter=new List<string>()
+        private static List<string> char_List =new List<string>()
         {
             "-","X","O"
         };
         //private Ship PlayerShips{get; set;}
 
 
-        //public List<string> fila_X = new List<string>();
-        public List<List<string>> Filas_Tablero = new List<List<string>>();
+        //public List<string> Row_X = new List<string>();
+        public List<List<string>> board_Rows = new List<List<string>>();
 
         //public Board(User Username)
         public Board()
@@ -38,28 +38,28 @@ namespace Proyecto
 #region ConstructorTablero
         public List<List<string>> Start_Board()
         {   
-            List<string> fila_X = new List<string>();          
+            List<string> Row_X = new List<string>();          
             for (int i = 0; i <= 10; i++)
             {
-                fila_X.Add(ABC[i]);
+                Row_X.Add(ABC[i]);
             }
-            Filas_Tablero.Add(fila_X);
+            board_Rows.Add(Row_X);
             //Esto recorre filas
             for (int y = 0; y < 10; y++)
             {
-                List<string> fila_N = new List<string>();
-                fila_N.Add(num_Filas[y]);
+                List<string> Row_N = new List<string>();
+                Row_N.Add(row_Num[y]);
                
                 //Esto recorre en cada fila, cada columna que la compone
                 for (int x = 0; x < 10; x++)
                 {
-                    fila_N.Add(lista_Caracter[0]);
+                    Row_N.Add(char_List[0]);
                 }
-                Filas_Tablero.Add(fila_N);
+                board_Rows.Add(Row_N);
             }
 
 
-            return Filas_Tablero;
+            return board_Rows;
         }
 #endregion ConstructorTablero
          /*
@@ -68,7 +68,7 @@ namespace Proyecto
             for (int z = 1; z <= 11; z++)
             for (int w = 1; w <= 11; w++)
             {
-                if Filas_Tablero[w] coordinate1
+                if board_Rows[w] coordinate1
                 {
 
                 } 
@@ -80,7 +80,7 @@ namespace Proyecto
 
         public void Print_Board()
         {
-            string Fila_I=null;
+            string Row_I="";
 
             //Recorro filas
             for (int y = 0; y < 11; y++)
@@ -92,51 +92,100 @@ namespace Proyecto
                     {
                         if (x==0)
                         {
-                            Fila_I=($"{Fila_I}{Filas_Tablero[y][x]:>6}");
+                            Row_I=($"{Row_I}{board_Rows[y][x]:>6}");
                         }
                         else if(x==1)
                         {
-                            Fila_I=($"{Fila_I}  {Filas_Tablero[y][x]:>6}");
+                            Row_I=($"{Row_I}  {board_Rows[y][x]:>6}");
                         }
                         else
                         {
-                            Fila_I=($"{Fila_I}  {Filas_Tablero[y][x]:>3}");
+                            Row_I=($"{Row_I}  {board_Rows[y][x]:>3}");
                         }
                     }
                     else if(y==10)
                     {
                         if (x==0)
                        {
-                           Fila_I=($"{Filas_Tablero[y][x]:>3}");
+                           Row_I=($"{board_Rows[y][x]:>3}");
                        }
                        else if (x==1)
                        {
-                           Fila_I=($"{Fila_I} {Filas_Tablero[y][x]:>2}");
+                           Row_I=($"{Row_I} {board_Rows[y][x]:>2}");
                        }
                        else
                        {
-                           Fila_I=($"{Fila_I}  {Filas_Tablero[y][x]:>2}");
+                           Row_I=($"{Row_I}  {board_Rows[y][x]:>2}");
                        }
                     } 
                     else
                     {
                        if (x==0)
                        {
-                           Fila_I=($"{Filas_Tablero[y][x]:>3}");
+                           Row_I=($"{board_Rows[y][x]:>3}");
                        }
                        else
                        {
-                           Fila_I=($"{Fila_I}  {Filas_Tablero[y][x]:>2}");
+                           Row_I=($"{Row_I}  {board_Rows[y][x]:>2}");
                        }
                    
                     }
                 }
-                Console.WriteLine(Fila_I);
+                Console.WriteLine(Row_I);
             }
 
 
         }
 
+        public void Edit_Board(string coord1, string coord2)
+        {
+            if (ABC.Contains(coord1.ToUpper()))
+            {
+                Console.WriteLine("Coord1 esta dentro de la lista");
+            }
+            else
+            {
+                Console.WriteLine("Coord1 no esta dentro de la lista");
+            }
+
+            bool hit;
+            hit = false;
+            int Index_Y;
+            int Index_X;
+
+            //Prueba de indices
+            int Index_Z;
+            Index_Z= ABC.IndexOf(coord1.ToUpper());
+            Console.WriteLine($"El indice de {coord1} es: {Index_Z}");
+            int Index_K;
+            Index_K= row_Num.IndexOf(coord2);
+            Console.WriteLine($"El indice de {coord2} es: {Index_K+1}");
+            board_Rows[Index_Z+1][Index_K+1]=char_List[1];
+
+
+            // board_Rows=[[/ABCDEFG][1-X--------][2----------][]]
+
+
+            for (int y = 0; y < 11; y++)
+            {
+                if (board_Rows[y][0]==coord2)
+                {
+                    Index_Y=y;
+                    for (int x = 0; x < 11; x++)
+                    {
+                        if (board_Rows[0][x]==coord1.ToUpper())
+                        {
+                            Index_X=x;
+                            hit=true;
+                            if (hit==true)
+                            {
+                                board_Rows[Index_Y][Index_X]=char_List[1];
+                            }
+                        }      
+                    }
+                }
+            } 
+        }
     }
 }
 
