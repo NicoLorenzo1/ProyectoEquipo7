8f84b0140877b3663f493d0ce2e9fc588a519271
JoseVarela8 <45233198+JoseVarela8@users.noreply.github.com>
2022-06-20 23:38:48 -0300

    Revert "Nico branch"

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,19 +7,38 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            User jose = new User("jose");
+            User user = new User("user");
+            User user2 = new User("user2");
+            Game game = new Game(user,user2,"classic");
+            game.StartGame();
 
-            Administrator administrator = Administrator.Instance;
-            administrator.UsersToPlay.Add(jose, "Classic");
+            //User user2 = new User("user1");
+            //User user4 = new User("user4");
 
-            //Game game = new Game("classic");
-            //game.AddUserToWaitList(user1);
-            //game.AddUserToWaitList(user2);
+            //Console.WriteLine(User.users["user"]);
+            //Console.WriteLine(user.Id);
+            // Console.WriteLine(user2.Id);
+            // Console.WriteLine(user4.Id);
 
-            Menu menu = new Menu();
-            menu.ShowMenu();
+            //TimeTrial timeTrial = new TimeTrial();
+            //timeTrial.FinishTimeGame();
+
+            //Menu menu = new Menu();
+            //menu.ShowMenu();
+        
+
+
+            //Console.WriteLine(Stadistics.playedGames.Count);
+
+
+
+            //QuickChat.SendPredefinedChat(1);
+            //QuickChat.AllMessages();
+            //Stadistics.ShowStats(user);
+
+
+
 
         }
     }
 }
-
