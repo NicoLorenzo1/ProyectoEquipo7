fefbb60a6c7739ab9048523364d4f14ad01471cc
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-06-26 21:08:46 -0300

    bot funcionando con main solucionado

--- src/Library/Bot.cs
+++ src/Library/Bot.cs
@@ -1,1 +1,47 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 //por hacer
+namespace Library
+{
+public abstract class Bot{
+            // El token provisto por Telegram al crear el bot. Mira el archivo README.md en la raíz de este repo para
+        // obtener indicaciones sobre cómo configurarlo.
+    public static string token;
+    // Configura la aplicación.
+    public static void Setup()
+    {
+        // Lee una variable de entorno NETCORE_ENVIRONMENT que si no existe o tiene el valor 'development' indica
+        // que estamos en un ambiente de desarrollo.
+        var developmentEnvironment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+        var isDevelopment =
+            string.IsNullOrEmpty(developmentEnvironment) ||
+            developmentEnvironment.ToLower() == "development";
+
+        var builder = new ConfigurationBuilder();
+        builder
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        // En el ambiente de desarrollo el token secreto del bot se toma de la configuración secreta
+        if (isDevelopment)
+        {
+            builder.AddUserSecrets<Bot>();
+        }
+
+        var configuration = builder.Build();
+
+        IServiceCollection services = new ServiceCollection();
+
+        // Mapeamos la implementación de las clases para  inyección de dependencias
+        services
+            .Configure<BotSecret>(configuration.GetSection(nameof(BotSecret)))
+            .AddSingleton<ISecretService, SecretService>();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var revealer = serviceProvider.GetService<ISecretService>();
+        token = revealer.Token;
+    }
+}
+}
+
---
+++ src/Library/Telegram/TelegramBot.cs
@@ -0,0 +1,91 @@
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+namespace Library{
+    public abstract class TelegramBot{
+        private static TelegramBotClient telegramClient;
+        private static IHandler firstHandler;
+
+        public static async void Start(){
+                telegramClient = new TelegramBotClient(Bot.token);
+
+                firstHandler =
+                    new HelloHandler(
+                    new GoodByeHandler(
+                    new PhotoHandler(telegramClient, null)
+                ));
+
+                var cts = new CancellationTokenSource();
+
+                // Comenzamos a escuchar mensajes. Esto se hace en otro hilo (en background). El primer método
+                // HandleUpdateAsync es invocado por el bot cuando se recibe un mensaje. El segundo método HandleErrorAsync
+                // es invocado cuando ocurre un error.
+                telegramClient.StartReceiving(
+                    HandleUpdateAsync,
+                    HandleErrorAsync,
+                    new ReceiverOptions()
+                    {
+                        AllowedUpdates = Array.Empty<UpdateType>()
+                    },
+                    cts.Token
+                );
+            Console.WriteLine($"Bot is up!");
+
+            Console.ReadLine();
+
+            // Terminamos el bot.
+            cts.Cancel();
+        }
+
+     
+        /// <summary>
+        /// Maneja las actualizaciones del bot (todo lo que llega), incluyendo mensajes, ediciones de mensajes,
+        /// respuestas a botones, etc. En este ejemplo sólo manejamos mensajes de texto.
+        /// </summary>
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // Sólo respondemos a mensajes de texto
+                if (update.Type == UpdateType.Message)
+                {
+                    await HandleMessageReceived(botClient, update.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                await HandleErrorAsync(botClient, e, cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// Maneja los mensajes que se envían al bot a través de handlers de una chain of responsibility.
+        /// </summary>
+        /// <param name="message">El mensaje recibido</param>
+        /// <returns></returns>
+        private static async Task HandleMessageReceived(ITelegramBotClient botClient, Message message)
+        {
+            Console.WriteLine($"Received a message from {message.From.FirstName} saying: {message.Text}");
+
+            string response = string.Empty;
+
+            firstHandler.Handle(message, out response);
+
+            if (!string.IsNullOrEmpty(response))
+            {
+                await telegramClient.SendTextMessageAsync(message.Chat.Id, response);
+            }
+        }
+
+        /// <summary>
+        /// Manejo de excepciones. Por ahora simplemente la imprimimos en la consola.
+        /// </summary>
+        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(exception.Message);
+            return Task.CompletedTask;
+        }   
+    }
+}
